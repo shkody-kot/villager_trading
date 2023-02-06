900685e378bcd200e43e0446e77a059f2ebae963
shkody-kot <shkodykot.35@gmail.com>
2023-02-06 10:41:23 -0800

    updated tables, added comments, added ON DELETE

--- villagers.sql
+++ villagers.sql
@@ -1,4 +1,9 @@
+-- Group 51: Ready for the SQL
+-- Members: Ira Belyaeva, Kierra Young
+
+-------------------------------------
 -- Create villagers table
+-------------------------------------
 CREATE OR REPLACE TABLE villagers (
 	villager_id 	int NOT NULL AUTO_INCREMENT,
 	trade_name 		varchar(45) NOT NULL,
@@ -8,26 +13,31 @@ CREATE OR REPLACE TABLE villagers (
 	FOREIGN KEY (trade_name) REFERENCES trades(name)
 );
 
+-------------------------------------
 -- Create items table
+-------------------------------------
 CREATE OR REPLACE TABLE items (
 	item_id		int NOT NULL AUTO_INCREMENT,
 	name		varchar(45) NOT NULL,
 	cost 		int NOT NULL,
 	amount 		int, 
 	trade_name	varchar(45) NOT NULL,
-	game_id		int NOT NULL,
 	PRIMARY KEY (item_id),
 	CONSTRAINT fk_trades_name FOREIGN KEY (trade_name) REFERENCES trades(name)
 );
 
+-------------------------------------
 -- Create trades table
+-------------------------------------
 CREATE OR REPLACE TABLE trades (
 	name		varchar(45) UNIQUE NOT NULL,
 	description	varchar(255) NOT NULL,
 	PRIMARY KEY (name)
 );
 
+-------------------------------------
 -- Create discounts table
+-------------------------------------
 CREATE OR REPLACE TABLE discount_effects (
 	discount_id	int NOT NULL AUTO_INCREMENT,
 	name 		varchar(45) NOT NULL,
@@ -35,14 +45,18 @@ CREATE OR REPLACE TABLE discount_effects (
 	PRIMARY KEY (discount_id)
 );
 
+-------------------------------------
 -- Create customers table
+-------------------------------------
 CREATE OR REPLACE TABLE customers (
 	customer_id	int NOT NULL AUTO_INCREMENT,
 	name		varchar(45) NOT NULL,
 	PRIMARY KEY (customer_id)
 );
 
+-------------------------------------
 -- Create transactions table
+-------------------------------------
 CREATE OR REPLACE TABLE transactions (
 	transaction_id	int NOT NULL AUTO_INCREMENT,
 	customer_id 	int NOT NULL,
@@ -55,16 +69,20 @@ CREATE OR REPLACE TABLE transactions (
 	CONSTRAINT fk__discounts_id FOREIGN KEY (discount_id) REFERENCES discount_effects (discount_id)
 );
 
+---------------------------------------------
 -- Create intersection: villager_has_items
+---------------------------------------------
 CREATE OR REPLACE TABLE villager_has_items (
 	villager_id		int NOT NULL,
 	item_id			int NOT NULL,
 	PRIMARY KEY (villager_id, item_id),
-	CONSTRAINT fk_villager_items_villager FOREIGN KEY (villager_id) REFERENCES villagers(villager_id),
-	CONSTRAINT fk_villager_items_item FOREIGN KEY (item_id) REFERENCES items(item_id)
+	CONSTRAINT fk_villager_items_villager FOREIGN KEY (villager_id) REFERENCES villagers(villager_id) ON DELETE CASCADE,
+	CONSTRAINT fk_villager_items_item FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE CASCADE
 );
 
+---------------------------------------------
 -- Create intersection: transaction_has_items
+---------------------------------------------
 CREATE OR REPLACE TABLE transaction_has_items (
 	transaction_id	int NOT NULL,
 	item_id			int NOT NULL,
