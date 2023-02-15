fd966fcba8d8e84150e36fb16b391a8771063ef1
shkody-kot <shkodykot.35@gmail.com>
2023-02-14 20:28:56 -0800

    sql files changed: dml.sql added

---
+++ dml.sql
@@ -0,0 +1,122 @@
+-- Group 51: Ready for the SQL
+-- Members: Ira Belyaeva, Kierra Young
+
+-- -----------------------------------
+-- Select relevant info from professions
+-- -----------------------------------
+SELECT * FROM professions;
+
+-- -----------------------------------
+-- Select info from villagers
+-- -----------------------------------
+SELECT villagers.name, villagers.trade_name, villagers.age, villagers.status, items.name
+AS item_name
+FROM villagers
+INNER JOIN villager_has_items ON villagers.villager_id = villager_has_items.villager_id
+INNER JOIN items ON villager_has_items.item_id = items.item_id
+ORDER BY villagers.name DESC;
+
+-- -----------------------------------
+-- Select info from items
+-- -----------------------------------
+SELECT name, cost, amount, trade_name FROM items
+GROUP BY trade_name
+ORDER BY trade_name ASC;
+
+-- -----------------------------------
+-- Select info from discounts
+-- -----------------------------------
+SELECT name, `percent` FROM discounts;
+
+-- -----------------------------------
+-- Select info from customers
+-- -----------------------------------
+SELECT name FROM customers;
+
+-- -----------------------------------
+-- Select info from transactions
+-- -----------------------------------
+SELECT transactions.transaction_id, customers.name AS customer, 
+villagers.name AS villager, discounts.name AS discount, transactions.total_price,
+items.name AS item, transaction_has_items.quantity
+FROM transactions
+LEFT JOIN discounts ON transactions.discount_id = discounts.discount_id
+LEFT JOIN villagers ON transactions.villager_id = villagers.villager_id
+LEFT JOIN customers ON transactions.customer_id = customers.customer_id
+LEFT JOIN transaction_has_items ON transactions.transaction_id = transaction_has_items.transaction_id
+LEFT JOIN items ON transaction_has_items.item_id = items.item_id
+ORDER BY transactions.transaction_id ASC;
+
+-- NOTE: items in [] are user-input into backend
+
+-- -----------------------------------
+-- Add a new item
+-- -----------------------------------
+INSERT INTO items (name, cost, amount, trade_name)
+VALUES ([name-typed], [cost-typed], [amount-typed], [trade_name-dropdown]);
+
+-- -----------------------------------
+-- Update an existing item
+-- -----------------------------------
+UPDATE items
+SET name = [name-dropdown], cost = [cost-typed], amount = [amount-typed]
+WHERE item_id = (ELECT item_id FROM items WHERE name = [item_name-dropdown]);
+
+-- -----------------------------------
+-- Delete an item
+-- -----------------------------------
+DELETE * FROM items WHERE name = [name-dropdown];
+-- -----------------------------------
+-- Add a new discount
+-- -----------------------------------
+INSERT INTO discounts (name, `percent`)
+VALUES ([name-typed], [discount_percent-typed]);
+
+-- -----------------------------------
+-- Add a new customer
+-- -----------------------------------
+INSERT INTO customers (name)
+VALUES ([name-typed]);
+
+-- -----------------------------------
+-- Add a new profession
+-- -----------------------------------
+INSERT INTO professions (name, description)
+VALUES ([name-typed], [description-typed]);
+
+-- -----------------------------------
+-- Add a new transaction
+-- -----------------------------------
+INSERT INTO transactions(customer_id, villager_id, discount_id, total_price)
+VALUES ((SELECT customer_id FROM customers WHERE name = [customer_name-typed]),
+(SELECT villager_id FROM villagers WHERE name = [villager_name-dropdown]),
+(SELECT discount_id FROM discounts WHERE name = [discounts-dropdown] OR IS NULL name),
+[total_price-calculated]);
+
+INSERT INTO transaction_has_items (transaction_id, item_id, quantity)
+VALUES ([transaction_id-stored], (SELECT item_id FROM items WHERE name = [item_name-dropdown]), [quantity-typed]);
+
+-- -----------------------------------
+-- Update an existing transaction
+-- -----------------------------------
+INSERT INTO transaction_has_items (transaction_id, item_id)
+VALUES (
+(SELECT transaction_id FROM transactions 
+	INNER JOIN villagers ON villagers.villager_id = transactions.villager_id
+	WHERE villager.name = [name-dropdown]), 
+(SELECT item_id FROM items WHERE name = [item_name-dropdown]), [quantity-typed]);
+
+-- -----------------------------------
+-- Add a new villager
+-- -----------------------------------
+INSERT INTO villagers (name, trade_name, age, status)
+VALUES ([name-typed], [trade-typed], [age-typed], [status-typed]);
+
+INSERT INTO villager_has_items (villager_id, item_id)
+VALUES ([villager_id-stored], (SELECT item_id FROM items WHERE name = [item_name-dropdown]));
+-- -----------------------------------
+-- Update an existing villager
+-- -----------------------------------
+UPDATE villagers 
+SET name = [name-dropdown], trade_name = [trade-dropdown], age = [age-typed], status = [status-typed],
+WHERE villager_id = (SELECT villager_id FROM villagers WHERE name = [name-dropdown]);
--- villagers.sql
+++ villagers.sql
@@ -3,7 +3,7 @@
 
 -- Drop all tables if exist
 SET FOREIGN_KEY_CHECKS = 0;
-DROP TABLE IF EXISTS trades;
+DROP TABLE IF EXISTS professions;
 DROP TABLE IF EXISTS villagers;
 DROP TABLE IF EXISTS items;
 DROP TABLE IF EXISTS discount_effects;
@@ -14,9 +14,9 @@ DROP TABLE IF EXISTS villager_has_items;
 SET FOREIGN_KEY_CHECKS = 1;
 
 -- -----------------------------------
--- Create trades table
+-- Create professions table
 -- -----------------------------------
-CREATE OR REPLACE TABLE trades (
+CREATE OR REPLACE TABLE professions (
 	name		varchar(45) UNIQUE NOT NULL,
 	description	varchar(255) NOT NULL,
 	PRIMARY KEY (name)
@@ -32,7 +32,7 @@ CREATE OR REPLACE TABLE villagers (
 	age 			int NOT NULL,
 	status 			varchar(45) NOT NULL,
 	PRIMARY KEY (villager_id),
-	FOREIGN KEY (trade_name) REFERENCES trades(name)
+	FOREIGN KEY (trade_name) REFERENCES professions(name)
 );
 
 -- -----------------------------------
@@ -45,13 +45,13 @@ CREATE OR REPLACE TABLE items (
 	amount 		int, 
 	trade_name	varchar(45) NOT NULL,
 	PRIMARY KEY (item_id),
-	CONSTRAINT fk_trades_name FOREIGN KEY (trade_name) REFERENCES trades(name)
+	CONSTRAINT fk_trades_name FOREIGN KEY (trade_name) REFERENCES professions(name)
 );
 
 -- -----------------------------------
 -- Create discounts table
 -- -----------------------------------
-CREATE OR REPLACE TABLE discount_effects (
+CREATE OR REPLACE TABLE discounts (
 	discount_id	int NOT NULL AUTO_INCREMENT,
 	name 		varchar(45) NOT NULL,
 	`percent`	int NOT NULL,
@@ -77,9 +77,9 @@ CREATE OR REPLACE TABLE transactions (
 	discount_id		int,
 	total_price		int NOT NULL,
 	PRIMARY KEY (transaction_id),
-	CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
-	CONSTRAINT fk_villager_id FOREIGN KEY (villager_id) REFERENCES villagers(villager_id),
-	CONSTRAINT fk_discount_id FOREIGN KEY (discount_id) REFERENCES discount_effects (discount_id)
+	CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customers(customer_id) ON DELETE CASCADE,
+	CONSTRAINT fk_villager_id FOREIGN KEY (villager_id) REFERENCES villagers(villager_id) ON DELETE SET NULL,
+	CONSTRAINT fk_discount_id FOREIGN KEY (discount_id) REFERENCES discounts (discount_id) ON DELETE SET NULL
 );
 
 -- -------------------------------------------
@@ -108,7 +108,7 @@ CREATE OR REPLACE TABLE transaction_has_items (
 -- -------------------------------------------
 -- Insert sample data: trades
 -- -------------------------------------------
-INSERT INTO trades (name, description)
+INSERT INTO professions (name, description)
 VALUES ("Armorer", "The armorer is a villager who sells armor and a shield, including enchanted diamond armor, in exchange for emeralds. The armorer is dressed in brown clothes, gloves are on his hands, and a goggle mask is on his head."),
 ("Farmer", "A farmer is one of the villagers who are engaged in growing various crops to supply the village with food. The farmer wears brown clothes with a belt, a straw hat on his head, and sells apples, bread, and other foods."),
 ("Fletcher", "The fletcher is a hunter who wears a brown hunting hat with a feather, an arrow in his belt, and a buckle on his belt that will change color depending on his skill. They sell arrows, flint, and other related materials."),
@@ -118,26 +118,26 @@ VALUES ("Armorer", "The armorer is a villager who sells armor and a shield, incl
 -- Insert sample data: items
 -- -------------------------------------------
 INSERT INTO items (name, cost, amount, trade_name)
-VALUES ("Nether Wart", 1000, 70, (SELECT name FROM trades WHERE name = "Cleric")),
-("Bread", 200, 100, (SELECT name FROM trades WHERE name = "Farmer")),
-("Arrow", 300, 50, (SELECT name FROM trades WHERE name = "Fletcher")),
-("Apple", 100, 200, (SELECT name FROM trades WHERE name = "Farmer")),
-("Chestplate", 3000, 2, (SELECT name FROM trades WHERE name = "Armorer"));
+VALUES ("Nether Wart", 1000, 70, (SELECT name FROM professions WHERE name = "Cleric")),
+("Bread", 200, 100, (SELECT name FROM professions WHERE name = "Farmer")),
+("Arrow", 300, 50, (SELECT name FROM professions WHERE name = "Fletcher")),
+("Apple", 100, 200, (SELECT name FROM professions WHERE name = "Farmer")),
+("Chestplate", 3000, 2, (SELECT name FROM professions WHERE name = "Armorer"));
 
 -- -------------------------------------------
 -- Insert sample data: villagers
 -- -------------------------------------------
 INSERT INTO villagers (name, trade_name, age, status)
-VALUES ("Denni", (SELECT name FROM trades WHERE name = "Armorer"), 76, "deceased"),
-("Ward", (SELECT name FROM trades WHERE name = "Farmer"), 56, "alive"),
-("Bones", (SELECT name FROM trades WHERE name = "Farmer"), 23, "alive"),
-("Wayfare", (SELECT name FROM trades WHERE name = "Fletcher"), 44, "alive"),
-("Glaser", (SELECT name FROM trades WHERE name = "Cleric"), 32, "alive");
+VALUES ("Denni", (SELECT name FROM professions WHERE name = "Armorer"), 76, "deceased"),
+("Ward", (SELECT name FROM professions WHERE name = "Farmer"), 56, "alive"),
+("Bones", (SELECT name FROM professions WHERE name = "Farmer"), 23, "alive"),
+("Wayfare", (SELECT name FROM professions WHERE name = "Fletcher"), 44, "alive"),
+("Glaser", (SELECT name FROM professions WHERE name = "Cleric"), 32, "alive");
 
 -- -------------------------------------------
 -- Insert sample data: discount_effects
 -- -------------------------------------------
-INSERT INTO discount_effects (name, `percent`)
+INSERT INTO discounts (name, `percent`)
 VALUES ("Hero", 30),
 ("RaidPrevention", 20),
 ("Bulk", 10);
@@ -155,7 +155,7 @@ VALUES ("Steve"),
 -- Insert sample data: transactions
 -- -------------------------------------------
 INSERT INTO transactions (customer_id, villager_id, discount_id, total_price)
-VALUES ((SELECT customer_id FROM customers WHERE name = "Steve"), (SELECT villager_id FROM villagers WHERE name = "Glaser"), (SELECT discount_id FROM discount_effects WHERE name = "Hero"), 3000),
+VALUES ((SELECT customer_id FROM customers WHERE name = "Steve"), (SELECT villager_id FROM villagers WHERE name = "Glaser"), (SELECT discount_id FROM discounts WHERE name = "Hero"), 3000),
 ((SELECT customer_id FROM customers WHERE name = "Kai"), (SELECT villager_id FROM villagers WHERE name = "Ward"), NULL, 2000),
 ((SELECT customer_id FROM customers WHERE name = "Noor"), (SELECT villager_id FROM villagers WHERE name = "Wayfare"), NULL, 7200);
 
@@ -166,8 +166,8 @@ INSERT INTO villager_has_items (villager_id, item_id)
 VALUES ((SELECT villager_id FROM villagers WHERE name = "Ward"), (SELECT item_id FROM items WHERE name = "Bread")),
 ((SELECT villager_id FROM villagers WHERE name = "Ward"), (SELECT item_id FROM items WHERE name = "Apple")),
 ((SELECT villager_id FROM villagers WHERE name = "Denni"), (SELECT item_id FROM items WHERE name = "Chestplate")),
-((SELECT villager_id FROM villagers WHERE name = "Wayfare"), (SELECT item_id FROM items WHERE name = "Nether Wart")),
-((SELECT villager_id FROM villagers WHERE name = "Glaser"), (SELECT item_id FROM items WHERE name = "Arrow"));
+((SELECT villager_id FROM villagers WHERE name = "Wayfare"), (SELECT item_id FROM items WHERE name = "Arrow")),
+((SELECT villager_id FROM villagers WHERE name = "Glaser"), (SELECT item_id FROM items WHERE name = "Nether Wart"));
 
 -- -------------------------------------------
 -- Insert sample data (intersection table): transaction_has_items
